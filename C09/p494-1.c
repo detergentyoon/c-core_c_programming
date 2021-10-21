a61cd970b89788c62d77a205d5ce5710ef310d40
detergentyoon <detergentyoon@gmail.com>
2021-10-21 21:53:16 +0900

    예제

---
+++ C09/p494-1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>// strlen()
+#include<ctype.h>// isspace()
+
+int main()
+{
+	char str[256];
+	int count = 0;
+
+	printf("문자열: ");
+	scanf_s("%[^\n]", str, sizeof(str)); // \n을 제외한 문자만 받음
+
+	int n = strlen(str);
+
+	for (int i = 0; i < n; i++)
+		if (isspace(str[i])) // 공백문자를 찾을때 마다 count++
+			count++;
+
+	printf("공백 문자의 개수: %d", count);
+
+	return 0;
+}
---
+++ C09/p494-2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<ctype.h>// isspace() 함수
+
+void remove_space(char* str) 
+{
+	char* pstr = str;
+
+	while (*str != 0)
+	{
+		// 공백문자를 제외하고 배열에 저장
+		if (isspace(*str) == 0) // isspace() = 공백이 아닌 경우 0 반환
+		{
+			if (pstr != str)
+				*pstr = *str;
+			pstr++;
+		}
+		str++;
+	}
+	*pstr = 0;
+}
+
+int main(void) 
+{
+	char str[256];
+
+	printf("문자열: ");
+	scanf_s("%[^\n]s", str, sizeof(str));
+
+	remove_space(str);
+
+	printf("공백 문자 제거후: %s", str);
+
+	return 0;
+}
